e3ae0c4e6a8e3ba65b87743a593d2538fc50aeda
Jinju Park <pjj7367@gmail.com>
2022-10-25 14:29:01 +0900

    [UPDATE] login

--- app/src/main/java/com/example/autism_care/Mypage.java
+++ app/src/main/java/com/example/autism_care/Mypage.java
@@ -6,6 +6,10 @@ import android.os.Bundle;
 
 public class Mypage extends AppCompatActivity {
 
+    // data : {'id':string, 'emotion':int, 'bool':BOOL, 'date':SYSDATE, 'type':int}
+    // emotion -> 0:기쁨, 1:당황, 2:불안, 3:슬픔, 4:분노, 5:상처
+    // quiz -> 0:얼굴로따라하기(사진+글씨), 1:얼굴로따라하기(사진), 2:얼굴로따라하기(글씨), 3:퀴즈로풀기
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
